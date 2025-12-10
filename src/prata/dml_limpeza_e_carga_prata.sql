80bf6f1e0d6ff28f6ff9efc33c6aec82402b30c6
Santiago956 <ppfsantiago@gmail.com>
2025-12-10 19:20:17 -0300

    Limpeza de dados: query para limpeza das tabelas do crm para a camada prata

--- src/prata/dml_limpeza_e_carga_prata.sql
+++ src/prata/dml_limpeza_e_carga_prata.sql
@@ -1,3 +1,5 @@
+PRINT '--- Inserindo dados na tabela prata.crm_cust_info'
+
 INSERT INTO prata.crm_cust_info (
 	cst_id,
 	cst_key,
@@ -30,3 +32,71 @@ FROM (
 	FROM bronze.crm_cust_info
 	WHERE cst_id IS NOT NULL) t -- Filtrando duplicatas de registros desatualizados
 WHERE flag_last = 1;
+
+PRINT '--- Inserindo dados na tabela prata.crm_prd_info'
+
+INSERT INTO prata.crm_prd_info (
+    prd_id,
+    cat_id,
+    prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    prd_end_dt
+)
+SELECT 
+prd_id,
+REPLACE(SUBSTRING(prd_key,1,5), '-', '_') AS cat_id,
+SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key,
+prd_nm,
+ISNULL(prd_cost, 0) AS prd_cost,
+CASE UPPER(TRIM(prd_line))
+    WHEN 'M' THEN 'Mountain'
+    WHEN 'R' THEN 'Road'
+    WHEN 'S' THEN 'Other Sales'
+    WHEN 'T' THEN 'Touring'
+    ELSE 'n/a'
+END AS prd_line,
+CAST(prd_start_dt AS DATE) AS prd_start_dt,
+CAST(LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info;
+
+PRINT '--- Inserindo dados na tabela prata.crm_sales_details'
+
+INSERT INTO prata.crm_sales_details (
+
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+
+)
+
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END AS sls_order_dt,
+	CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END AS sls_ship_dt,
+	CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL  --- Aplicando lógica de negócio para resolução de valores nulos e/ou negativos/incoerentes
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END AS sls_due_dt,
+	CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE WHEN sls_price IS NULL OR sls_price <= 0 THEN sls_sales / NULLIF(sls_quantity, 0)
+		ELSE sls_price
+	END AS sls_price
+	
+FROM bronze.crm_sales_details;
